85faf5899ba3e84d69f284d45be15de13a6c17ee
h1alexbel <aliaksei.bialiauski@hey.com>
2024-06-21 18:56:27 +0300

    feat(#37): zip

---
+++ steps/zip.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# The MIT License (MIT)
+#
+# Copyright (c) 2024 Aliaksei Bialiauski
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+set -e
+set -o pipefail
+
+# Move source.
+mv repos.csv clusters/source/repos.csv
+mv numerical.csv clusters/source/numerical.csv
+mv texts.csv clusters/source/texts.csv
+mv text-embeddings.csv clusters/source/text-embeddings.csv
+mv similar.csv clusters/source/similar.csv
+mv mix.csv clusters/mix.csv
+
+# Zip results.
+zip -rv clusters-"$(date +"%Y-%m-%d-%H:%M")".zip clusters
