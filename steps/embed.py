0589f91b15f776f8f4195e5ad2af79da2909c078
h1alexbel <aliaksei.bialiauski@hey.com>
2024-06-27 14:43:42 +0300

    feat(#29): puzzle for hed 500

--- steps/embed.py
+++ steps/embed.py
@@ -28,7 +28,12 @@ print("Preparing for generating embeddings...")
 checkpoint = os.environ["INFERENCE_CHECKPOINT"]
 token = os.environ["HF_TOKEN"]
 source = "texts"
-frame = pd.read_csv(f"{source}.csv")
+# @todo #29:45min Generate embeddings for all the text in texts.csv.
+#  Some of the texts generated after textualize_repos.py cannot be processed
+#  on the embeddings endpoint that lead to raising decoding exception:
+#  'json.decoder.JSONDecodeError: Expecting value: line 1 column 1 (char 0)'.
+#  We should fix that and remove `head(500)` frame limitation.
+frame = pd.read_csv(f"{source}.csv").head(500)
 candidates = ["text"]
 print(f"Checkpoint: {checkpoint}")
 print(f"CSV Source: {source}")
@@ -37,6 +42,7 @@ print(f"Candidates: {candidates}")
 for candidate in candidates:
     print(f"Generating embeddings for {candidate}...")
     embeddings = pd.DataFrame(infer(frame[candidate].tolist(), checkpoint, token))
+    embeddings.insert(0, 'repo', frame["repo"])
     embeddings.to_csv(f"{candidate}-embeddings.csv", index=False)
     print(f"Generated embeddings for {candidate}:")
     print(embeddings)
